a6f9898d5c9e377837bde94b844e02821ff78d07
wangnantu <wangnantu@hotmail.com>
2013-05-11 16:10:25 +0200

    add

---
+++ src/se/uu/it/bean/ProductBean.java
@@ -0,0 +1,127 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.uu.it.bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+/**
+ *
+ * @author wnt
+ */
+public class ProductBean {
+
+
+    private int id;
+    private String name;
+    private int price;
+    private ArrayList<String> componentsList;
+    
+    public ProductBean() throws Exception{
+        String url = "jdbc:mysql://localhost:3306/webshop?user=root&password=hjkl;'";
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        componentsList = new ArrayList<String>();
+        try{
+             // get a database connection and load the JDBC-driver
+
+            Class.forName("com.mysql.jdbc.Driver");
+            conn=DriverManager.getConnection(url);
+            
+            // create SQL statements to load the items into the list
+            // each item is a object
+            stmt = conn.createStatement();
+            String sql = "select item_id from product_item where product_id = "+id;
+            rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                int item_id =rs.getInt("item_id");
+                String sql2 = "select name from item where id = "+item_id;
+                ResultSet rs2 = stmt.executeQuery(sql2);
+                String item_name = rs2.getString("name");
+                componentsList.add(item_name);
+                
+            }
+            
+        }catch(SQLException sqle){
+            throw new Exception(sqle);
+        }
+          finally{
+ 	    try{
+              rs.close();
+            }
+            catch(Exception e) {}
+            try{
+              stmt.close();
+            }
+	    catch(Exception e) {}
+            try {
+              conn.close();
+            }
+            catch(Exception e){}
+        }
+    }
+    
+    
+        public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public int getPrice() {
+        return id;
+    }
+
+    public void setPrice(int id) {
+        this.id = id;
+    }
+    
+    public ArrayList<String> getComponents() {
+        
+        return componentsList;
+    }
+    
+    public String getXml(){
+        StringBuffer xmlOut = new StringBuffer();
+         xmlOut.append("<product>");
+         xmlOut.append("<id>");
+         xmlOut.append(id);
+         xmlOut.append("</id>");
+         xmlOut.append("<name>");               
+         xmlOut.append(name);
+         xmlOut.append("</name>");
+         xmlOut.append("<price>");
+         xmlOut.append(price);
+         xmlOut.append("</price>");
+         
+         Iterator it = componentsList.iterator();
+          while(it.hasNext()){
+         xmlOut.append("<component>");
+         xmlOut.append((String)it.next());
+         xmlOut.append("</component>");
+          }
+          xmlOut.append("<product>");
+        return xmlOut.toString();
+        
+    }
+    
+}
--- src/se/uu/it/bean/ProductListBean.java
+++ src/se/uu/it/bean/ProductListBean.java
@@ -1,5 +1,6 @@
 /*
- * ItemListBean.java
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
 package se.uu.it.bean;
 
@@ -15,38 +16,37 @@ import java.util.Iterator;
  *
  * @author wnt
  */
-public class ItemListBean {
+public class ProductListBean {
     
     private String url = null;
-    private  ArrayList itemList;
+    private  ArrayList productList;
     
-      public ItemListBean() throws Exception{
+    public ProductListBean()throws Exception{
       this("jdbc:mysql://localhost:3306/webshop?user=root&password=hjkl;'");
       }
-    public ItemListBean(String _url) throws Exception{
+    public ProductListBean(String _url)throws Exception{
         url = _url;
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
-        itemList = new ArrayList();
+        productList = new ArrayList();
         try{
              // get a database connection and load the JDBC-driver
 
             Class.forName("com.mysql.jdbc.Driver");
             conn=DriverManager.getConnection(url);
             
-            // create SQL statements to load the items into the list
-            // each item is a object
+            // create SQL statements to load the products into the list
+            // each product is a object
             stmt = conn.createStatement();
-            String sql = "select *from item";
+            String sql = "select *from product";
             rs = stmt.executeQuery(sql);
             while(rs.next()){
-                ItemBean item = new ItemBean();
-                item.setId(rs.getInt("id"));
-                item.setName(rs.getString("name"));
-                item.setPrice(rs.getInt("price"));
-                item.setStock(rs.getInt("stock"));
-                itemList.add(item);
+                ProductBean product = new ProductBean();
+                product.setId(rs.getInt("id"));
+                product.setName(rs.getString("name"));
+                product.setPrice(rs.getInt("price"));
+                productList.add(product);
             }
             
         }catch(SQLException sqle){
@@ -68,32 +68,31 @@ public class ItemListBean {
         }
         
     }
-    //return the itemList
-    public ArrayList<ItemBean>  getItemList(){
-        return itemList;
+
+    //return the productList
+    public ArrayList<ProductBean> getProductList(){
+        return productList;
     }
     
-    //create an XML file from the itemList
-    
+    //create an XML file from productList
     public String getXml(){
-        ItemBean item = null;
-        Iterator it = itemList.iterator();
+        ProductBean product = null;
+        Iterator it = productList.iterator();
         StringBuffer buff = new StringBuffer();
-        buff.append("<itemlist>");
+        buff.append("<productlist>");
         while(it.hasNext()){
-            item = (ItemBean)it.next();
-            buff.append(item.getXml());
+            product = (ProductBean)it.next();
+              buff.append(product.getXml());
         }
-        buff.append("</itemlist>");
-        
+          buff.append("</productlist>");
+          
         return buff.toString();
-        
     }
     
      public static void main(String[] args){
         try{
-	    ItemListBean ilb = new ItemListBean();
-	    System.out.println(ilb.getXml());
+	   ProductListBean plb = new ProductListBean();
+	    System.out.println(plb.getXml());
         }
         catch(Exception e){
 	    System.out.println(e.getMessage());
